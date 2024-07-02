4616dfe8e158714846cb8d55cee0a695f91ccb17
Pashidy <eldafrancis@yahoo.com>
2024-07-02 08:52:30 +0100

    Lockboxes Task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,16 +8,15 @@ def canUnlockAll(boxes):
     """
     The working script
     """
+    x = len(boxes)
+    item = [False] * x
+    item[0] = True
+    stack = [0]
 
-
-if (type(boxes) is not list or len(boxes) == 0):
-    return False
-    for k in range(1, len(boxes) - 1):
-        unlocked = False
-        for i in range(len(boxes)):
-            unlocked = k in boxes[i] and k != i
-            if unlocked:
-                break
-        if unlocked is False:
-            return unlocked
-    return True
+    while stack:
+        box = stack.pop()
+        for each in boxes[box]:
+            if each >= 0 and each < x and not item[each]:
+                item[each] = True
+                stack.append(each)
+    return all(item)
