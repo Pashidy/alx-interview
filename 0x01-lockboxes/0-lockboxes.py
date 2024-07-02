b655bb2a3ad5a3b891742b32f1ef170fb99dd91d
Pashidy <eldafrancis@yahoo.com>
2024-07-02 08:24:26 +0100

    Lockboxes Task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,29 +10,14 @@ def canUnlockAll(boxes):
     """
 
 
-if not isinstance(boxes, list) or len(boxes) == 0:
+if (type(boxes) is not list or len(boxes) == 0):
     return False
-
-    n = len(boxes)
-    opened = [False] * n
-    opened[0] = True  # The first box is always open
-    stack = [0]  # Start with the first box
-
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if key < n and not opened[key]:
-                opened[key] = True
-                stack.append(key)
-
-    return all(opened)
-
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
