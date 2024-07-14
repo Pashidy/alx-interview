952488ec993b5bfa015312240b168f1148436a13
Pashidy <eldafrancis@yahoo.com>
2024-07-14 23:07:26 +0100

    Min Ops Task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,17 +5,21 @@ To calculate the min No. of Ops needed to result in exactly n H characters
 
 
 def minOperations(n):
+    """
+    Min Ops
+    """
+
     if n <= 1:
         return 0
 
     operations = 0
-    divisor = 2
+    factor = 2
 
     while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
 
 
 return operations
