36a539d76e040e729d7d2d500cad12f3f9ac1cbe
Pashidy <eldafrancis@yahoo.com>
2024-07-15 04:17:00 +0100

    Min Ops Task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 This module defines a fxn to calculate the min no. of Ops
 needed to get exactly n 'H' Xters in a text file starting with a single 'H'.
@@ -18,10 +19,8 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-
     operations = 0
     factor = 2
-
 
     while n > 1:
         while n % factor == 0:
