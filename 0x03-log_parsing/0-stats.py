09510aa8966f7b2d90822ca9385551311e0640b3
Pashidy <eldafrancis@yahoo.com>
2024-07-17 23:24:02 +0100

    log parsing task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,53 +3,54 @@
 A script that reads stdin line by line and computes metrics.
 """
 import sys
+import signal
 
-
-def print_msg(dict_sc, total_file_size):
+def printsts(dic, size):
     """
-    Method to print statistics.
-
-    Args:
-        dict_sc: Dictionary of status codes and their counts.
-        total_file_size: Total size of all files processed.
+    Prints information
     """
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+    print("File size: {:d}".format(size))
+    for key in sorted(dic.keys()):
+        if dic[key] != 0:
+            print("{}: {:d}".format(key, dic[key]))
 
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
-total_file_size = 0
-counter = 0
-dict_sc = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+count = 0
+size = 0
+
+def signal_handler(sig, frame):
+    """
+    Handle the interrupt signal to print the final statistics
+    """
+    printsts(sts, size)
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
+        stlist = line.split()
 
-        if len(parsed_line) > 2:
-            counter += 1
+        if len(stlist) < 2:
+            continue
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])
-                code = parsed_line[1]
+        count += 1
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+        try:
+            size += int(stlist[-1])
+        except ValueError:
+            pass
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+        if stlist[-2] in sts:
+            sts[stlist[-2]] += 1
 
-# Print any remaining statistics when the script ends naturally
-print_msg(dict_sc, total_file_size)
+        if count % 10 == 0:
+            printsts(sts, size)
+
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
+
+printsts(sts, size)
