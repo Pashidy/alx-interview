3f9f811e5c4e2c61c2c0f1ec1e9a3ac0829e0738
Pashidy <eldafrancis@yahoo.com>
2024-07-17 23:14:03 +0100

    log parsing task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,7 +3,6 @@
 A script that reads stdin line by line and computes metrics.
 """
 import sys
-import signal
 
 
 def print_msg(dict_sc, total_file_size):
@@ -19,6 +18,7 @@ def print_msg(dict_sc, total_file_size):
         if val != 0:
             print("{}: {}".format(key, val))
 
+
 total_file_size = 0
 counter = 0
 dict_sc = {
@@ -32,37 +32,24 @@ dict_sc = {
     "500": 0
 }
 
-
-def signal_handler(sig, frame):
-    """
-    Handle the interrupt signal to print the final statistics.
-    """
-    print_msg(dict_sc, total_file_size)
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
 try:
     for line in sys.stdin:
         parsed_line = line.split()
-        if len(parsed_line) != 10:
-            continue
+        parsed_line = parsed_line[::-1]
 
-        counter += 1
-        total_file_size += int(parsed_line[-1])
-        code = parsed_line[-2]
+        if len(parsed_line) > 2:
+            counter += 1
 
-        if code in dict_sc:
-            dict_sc[code] += 1
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-        if counter == 10:
-            print_msg(dict_sc, total_file_size)
-            counter = 0
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
 
-except KeyboardInterrupt:
-    print_msg(dict_sc, total_file_size)
-    sys.exit(0)
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
 
 # Print any remaining statistics when the script ends naturally
 print_msg(dict_sc, total_file_size)
