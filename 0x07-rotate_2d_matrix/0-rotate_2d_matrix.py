f137688abca7e9b38f3d64efc501ab5c7b08d009
Pashidy <eldafrancis@yahoo.com>
2024-08-18 00:21:59 +0100

    Matrix task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,18 +9,9 @@ def rotate_2d_matrix(matrix):
     """
     n = len(matrix)
 
-    # Transpose the matrixfor i inrange(n):
-    for j inrange(i, n):
-        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        for j in range(i, n):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
 
-    # Reverse each rowfor i inrange(n):
-        matrix[i].reverse()
-
-
-if __name__ == "__main__":
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
-
-    rotate_2d_matrix(matrix)
-    print(matrix)
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
