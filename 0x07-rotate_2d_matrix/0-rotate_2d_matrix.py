115d5312eb4d2494a7b7e9653a6f45c5f11dc640
Pashidy <eldafrancis@yahoo.com>
2024-08-18 00:53:08 +0100

    Matrix task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Rotates a 2d matrix"""
+
 
 def rotate_2d_matrix(matrix):
     """
@@ -12,6 +14,6 @@ def rotate_2d_matrix(matrix):
 if __name__ == '__main__':
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-    """ rotate_2d_matrix(matrix) """
+    """The matrix"""
     rotate_2d_matrix(matrix)
     print(matrix)
