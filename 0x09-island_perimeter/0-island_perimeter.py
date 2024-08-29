307f52eb2c4e634380bccd27c723c7efecdc102b
Pashidy <eldafrancis@yahoo.com>
2024-08-29 23:58:14 +0100

    Island Perimeter Task

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,20 +5,14 @@ Iteration of a fxn that returns the perimeter of the island described in grid
 
 
 def island_perimeter(grid):
+    """The main code"""
     perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-
-    for i in range(rows):
-        for j in range(cols):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                # Start with 4 sides for each land cell
                 perimeter += 4
-
-                # Subtract 1 for each shared side with adjacent land cells
-                if i > 0 and grid[i - 1][j] == 1:  # Check cell above
-                    perimeter -= 2  # Shared side with the above cell
-                if j > 0 and grid[i][j - 1] == 1:  # Check cell to the left
-                    perimeter -= 2  # Shared side with the left cell
-
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
     return perimeter
