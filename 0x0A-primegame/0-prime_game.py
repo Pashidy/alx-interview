19d05fe2df3ae28d06fc06bf23f650a477a8b6e6
Pashidy <eldafrancis@yahoo.com>
2024-09-05 19:43:16 +0100

    primegame task

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,31 +3,22 @@
 
 
 def sieve_of_eratosthenes(max_n):
-    """
-    Generate a list of primes up to max_n using the Sieve of Eratosthenes.
-    """
+    """Generate a list indicating prime status up to max_n using Sieve of Eratosthenes."""
     is_prime = [True] * (max_n + 1)
     is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
     for i in range(2, int(max_n ** 0.5) + 1):
         if is_prime[i]:
             for j in range(i * i, max_n + 1, i):
                 is_prime[j] = False
-    return [i for i in range(max_n + 1) if is_prime[i]]
+    return is_prime
 
 
-def count_primes(n, primes):
-<<<<<<< HEAD
-    """Count how many primes exist from 1 to n using the primes list."""
-=======
-    """
-    Count how many primes exist from 1 to n using the primes list.
-    """
->>>>>>> ae7692d06a0b1277fc9f19f3a1557cf67e114346
+def count_primes_up_to_n(is_prime, n):
+    """Count how many primes exist from 1 to n using the is_prime list."""
     count = 0
-    for prime in primes:
-        if prime > n:
-            break
-        count += 1
+    for i in range(1, n + 1):
+        if is_prime[i]:
+            count += 1
     return count
 
 
@@ -35,16 +26,15 @@ def isWinner(x, nums):
     if x < 1 or not nums:
         return None
 
-    # Determine the maximum number in nums to generate primes up to that number
     max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
+    is_prime = sieve_of_eratosthenes(max_n)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        prime_count = count_primes(n, primes)
-        # If the count of primes is odd, Maria wins, otherwise Ben wins
+        prime_count = count_primes_up_to_n(is_prime, n)
+        # If the number of primes is odd, Maria wins; if even, Ben wins.
         if prime_count % 2 == 1:
             maria_wins += 1
         else:
